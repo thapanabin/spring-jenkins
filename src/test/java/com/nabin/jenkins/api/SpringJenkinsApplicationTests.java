1fde1aae4b989f51f31870f42c84580d84617af3
thapanabin <thapanabincr7@outlook.com>
2020-04-11 01:32:13 -0500

    error solved

--- src/test/java/com/nabin/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/nabin/jenkins/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ class SpringJenkinsApplicationTests {
 
 	Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
-	public  contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case executing");
 		logger.info("Test case executing second log statement ...");
 		assertEquals(true, true);
