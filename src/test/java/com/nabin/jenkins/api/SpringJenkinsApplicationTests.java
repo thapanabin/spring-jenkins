d88dfe75aef13b3e06f0a3868f2f68fd529ed24b
thapanabin <thapanabincr7@outlook.com>
2020-04-11 18:19:48 -0500

    removed logger statements

--- src/test/java/com/nabin/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/nabin/jenkins/api/SpringJenkinsApplicationTests.java
@@ -14,7 +14,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing");
-		logger.info("Test case executing second log statement ...");
 		assertEquals(true, true);
 	}
 
